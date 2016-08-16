44236f75f8165e3e1938728e35edfdae6b79b0c9
Chris Anderson <christopher.anderson@gmail.com>
2016-08-16 18:28:39 -0400

    Remove route fitness and add newer api values of total_idle_time and total_travel_time

--- lib/routific/route.rb
+++ lib/routific/route.rb
@@ -1,18 +1,33 @@
 module RoutificApi
   # This class represents the resulting route returned by the Routific API
   class Route
-    attr_reader :status, :fitness, :unserved, :vehicleRoutes
+    attr_reader :status, :unserved, :vehicleRoutes, :total_travel_time, :total_idle_time
 
     # Constructor
-    def initialize(status, fitness, unserved)
+    def initialize(status:, solution: {}, unserved: {}, total_travel_time: 0, total_idle_time: 0)
       @status = status
-      @fitness = fitness
       @unserved = unserved
-      @vehicleRoutes = Hash.new()
+      @total_idle_time = total_idle_time
+      @total_travel_time = total_travel_time
+
+      add_solution(solution)
+    end
+
+    def add_solution(solution)
+      @vehicleRoutes = {}
+
+      solution.each do |vehicle_name, way_points|
+        # Get all way points for this vehicle
+        way_points.each do |waypoint_info|
+          # Get all information for this way point
+          way_point = RoutificApi::WayPoint.new(waypoint_info)
+          add_way_point(vehicle_name, way_point)
+        end
+      end
     end
 
     # Adds a new way point for the specified vehicle
-    def addWayPoint(vehicle_name, way_point)
+    def add_way_point(vehicle_name, way_point)
       if @vehicleRoutes[vehicle_name].nil?
         # No previous way point was added for the specified vehicle, so create a new array
         @vehicleRoutes[vehicle_name] = []
@@ -21,26 +36,26 @@ module RoutificApi
       @vehicleRoutes[vehicle_name] << way_point
     end
 
+    def number_of_unserved
+      @number_of_unserved ||= unserved.count
+    end
+
     class << self
       # Parse the JSON representation of a route, and return it as a Route object
-      def parse(routeJson)
-        status = routeJson["status"]
-        fitness = routeJson["fitness"]
-        unserved = routeJson["unserved"]
-        route = RoutificApi::Route.new(status, fitness, unserved)
+      def parse(data)
+        data = process_data(data)
+        RoutificApi::Route.new(data)
+      end
 
-        # Get way points for each vehicles
-        routeJson["solution"].each do |vehicle_name, way_points|
-          # Get all way points for this vehicle
-          way_points.each do |waypoint_info|
-            # Get all information for this way point
-            way_point = RoutificApi::WayPoint.new(waypoint_info)
-            route.addWayPoint(vehicle_name, way_point)
-          end
+      def process_data(hash)
+        hash.keys.each do |key|
+          hash[(key.to_sym rescue key) || key] = hash.delete(key)
         end
 
-        # Return the resulting Route object
-        route
+        hash.delete(:num_unserved)
+        hash.delete(:unserved) if hash[:unserved] == nil
+
+        hash
       end
     end
   end
